e8fde34932501a24b9cb012caa3781a4170a5126
erickgalindosc-hub <erickgalindosc@gmail.com>
2026-07-24 14:47:55 -0500

    codificacion app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 import express from 'express';
+
+
+const express = require('express');  
+const app = express();  
+const port = 3000; 
+
+app.get("/", (_, res) => { 
+    res.send("Aprendicez ficha 3407186"); 
+}); 
+
+app.listen(port, () => { 
+    console.log( `Servidor en funcionamiento en el puerto: `); 
+});
